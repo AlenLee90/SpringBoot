92adb27c8d1ab5c9fbae9f834831c2df50ffa19b
山田和夫 <yamadakazuo@MacBook-Pro-4.local>
2018-10-01 19:20:58 +0900

    first commit

---
+++ src/main/java/com/FittingRoomServer/FittingRoomServer/FittingRoomServerApplication.java
@@ -0,0 +1,13 @@
+package com.FittingRoomServer.FittingRoomServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FittingRoomServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FittingRoomServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/FittingRoomServer/FittingRoomServer/FittingRoomServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.FittingRoomServer.FittingRoomServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FittingRoomServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
